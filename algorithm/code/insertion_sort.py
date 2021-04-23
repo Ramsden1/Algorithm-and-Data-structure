65c391800695b6715eef386d977febe5208c435b
Ramsden1 <47860545+Ramsden1@users.noreply.github.com>
2021-04-23 14:58:29 +0900

    Update insertion_sort.py

--- algorithm/code/insertion_sort.py
+++ algorithm/code/insertion_sort.py
@@ -1,4 +1,5 @@
-def insertion_sort(array):
+def insertion_sort_a(array):
+	# asending 
     for j in range(1, len(array)):
         key = array[j]
         i = j - 1
@@ -8,5 +9,18 @@ def insertion_sort(array):
         array[i+1] = key
 	return array
 
-a = insertion_sort([5,2,4,6,1,3])
-print(a)
+def insertion_sort_d(array):
+	#desending
+    for i in range(1, len(array)):
+        key = array[i]
+        j = i - 1
+        while j >= 0 and array[j] < key:
+            array[j+1] = array[j]
+            j -= 1
+        array[j+1] = key
+
+    return array
+
+a = insertion_sort_a([6,2,1,6,9,5,4,5,3])
+b = insertion_sort_d([6,2,1,6,9,5,4,5,3])
+print(a,b)
